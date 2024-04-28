892bf0135667aec9aa6748cc8c42ca1e908c1390
dnunezf <david.a.nunez27@outlook.com>
2024-04-27 19:55:05 -0600

    CLASE TEMPLATE

---
+++ Proyecto1/ContenedorFacturas.h
@@ -0,0 +1,27 @@
+#pragma once
+#include"Nodo.h"
+#include"Factura.h"
+#include"Apoyo.h"
+
+template<class Factura>
+
+class ConFactura {
+private:
+	Nodo<Factura>* facturas;
+
+public:
+	ConFactura();
+	ConFactura(const ConFactura&);
+	virtual ~ConFactura();
+};
+
+template<class Factura>
+ConFactura<Factura>::ConFactura() : facturas(NULL) {}
+
+template<class Factura>
+ConFactura<Factura>::ConFactura(const ConFactura&) {}
+
+template<class Factura>
+ConFactura<Factura>::~ConFactura() {
+
+}
