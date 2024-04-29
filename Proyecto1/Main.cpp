940eeb42967f132597e1beef88b60b5e96462f08
Angel-Leiva <angelleiva3hola@gmail.com>
2024-04-28 18:20:50 -0600

    Fin

--- Proyecto1/Main.cpp
+++ Proyecto1/Main.cpp
@@ -3,8 +3,10 @@
   20 de abril de 2024
 
   Autores:
-  Ángel Arián Leiva Abarca ()
+  Ángel Arián Leiva Abarca
+  Cedula: 119340740
   David Alberto Núñez Franco
+  Cedula: 119080008
 
   Universidad Nacional de Costa Rica, Escuela de Informática
   EIF204-G01, PROGRAMACIÓN II
